b7b2820ba489bcd848dfef432b4dbe6b77cbb4cd
cloud_flyer <103324739+linjt37@users.noreply.github.com>
2024-01-26 15:49:37 +0800

    Update predictor.py

--- roop/predictor.py
+++ roop/predictor.py
@@ -21,11 +21,11 @@ def clear_predictor() -> None:
 
 
 def predict_frame(target_frame: Frame) -> bool:
-    retrun False
+    return False
 
 
 def predict_image(target_path: str) -> bool:
-    retrun False
+    return False
 
 def predict_video(target_path: str) -> bool:
-    retrun False
+    return False
